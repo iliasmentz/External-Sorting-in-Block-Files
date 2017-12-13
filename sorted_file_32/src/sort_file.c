1556c172247a855c45e51711768c687cf404d534
Ilias Mentzelos <sdi1400106@di.uoa.gr>
2017-12-13 14:42:21 +0200

    First commit of the project

---
+++ sorted_file_32/src/sort_file.c
@@ -0,0 +1,48 @@
+#include "sort_file.h"
+
+SR_ErrorCode SR_Init() {
+  // Your code goes here
+
+  return SR_OK;
+}
+
+SR_ErrorCode SR_CreateFile(const char *fileName) {
+  // Your code goes here
+
+  return SR_OK;
+}
+
+SR_ErrorCode SR_OpenFile(const char *fileName, int *fileDesc) {
+  // Your code goes here
+
+  return SR_OK;
+}
+
+SR_ErrorCode SR_CloseFile(int fileDesc) {
+  // Your code goes here
+
+  return SR_OK;
+}
+
+SR_ErrorCode SR_InsertEntry(int fileDesc,	Record record) {
+  // Your code goes here
+
+  return SR_OK;
+}
+
+SR_ErrorCode SR_SortedFile(
+  const char* input_filename,
+  const char* output_filename,
+  int fieldNo,
+  int bufferSize
+) {
+  // Your code goes here
+
+  return SR_OK;
+}
+
+SR_ErrorCode SR_PrintAllEntries(int fileDesc) {
+  // Your code goes here
+
+  return SR_OK;
+}
