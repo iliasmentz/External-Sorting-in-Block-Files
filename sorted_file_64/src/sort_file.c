043a3d9f12f7b8ade32b1dab2a11141e67494f81
Ilias Mentzelos <sdi1400106@di.uoa.gr>
2018-01-07 15:38:49 +0200

    Everything setted up except from sort.

--- sorted_file_64/src/sort_file.c
+++ sorted_file_64/src/sort_file.c
@@ -18,6 +18,40 @@ BF_Block * block;
 int record_size = 0;
 int max_records = 0;
 
+void SR_WriteRecord(void * data, Record record)
+{
+    /*write the id*/
+    memcpy(data, &record.id, sizeof(record.id));
+    /*write the name*/
+    data += sizeof(record.id);
+    strcpy(data, record.name);
+    /*write the surname*/
+    data += sizeof(record.name);
+    strcpy(data, record.surname);
+    /*write the city*/
+    data += sizeof(record.surname);
+    strcpy(data, record.city);
+}
+
+void SR_ReadRecord(void * data, Record * record)
+{
+    /*read id*/
+    memcpy(&record->id, data, sizeof(record->id));
+    /*read name*/
+    data += sizeof(record->id);
+    memcpy(&record->name, data, sizeof(record->name));
+    /*read surname*/
+    data += sizeof(record->name);
+    memcpy(&record->surname, data, sizeof(record->surname));
+    /*read surname*/
+    data += sizeof(record->surname);
+    memcpy(&record->city, data, sizeof(record->city));
+}
+
+void SR_PrintRecords(Record record)	{
+	printf("|ID: %8d|, |Name: %15s|, |Surname: %15s|, |City: %20s| \n",
+     record.id, record.name, record.surname, record.city);
+}
 
 SR_ErrorCode SR_Init() {
   // Your code goes here
@@ -35,8 +69,8 @@ SR_ErrorCode SR_CreateFile(const char *fileName) {
 
   int fd;
 
-  CALL_OR_DIE(BF_CreateFile(filename));
-  CALL_OR_DIE(BF_OpenFile(filename, &fd));
+  CALL_OR_DIE(BF_CreateFile(fileName));
+  CALL_OR_DIE(BF_OpenFile(fileName, &fd));
   CALL_OR_DIE(BF_AllocateBlock(fd, block));
 
 
@@ -95,6 +129,57 @@ SR_ErrorCode SR_CloseFile(int fileDesc) {
 SR_ErrorCode SR_InsertEntry(int fileDesc,	Record record) {
   // Your code goes here
 
+  /*We get the block count in order to find the last one*/
+  int count;
+
+  CALL_OR_DIE(BF_GetBlockCounter(fileDesc, &count));
+
+  if(record_size <= 0)
+    return SR_ERROR;
+
+  char* data;
+  if (count > 1)
+  {   /* Get the Last Block*/
+      CALL_OR_DIE(BF_GetBlock(fileDesc, count-1, block));
+      data = BF_Block_GetData(block);
+      int record_count;
+      memcpy(&record_count, data, sizeof(int));
+      /*We check if the block has space for new record*/
+      if(record_count < max_records)
+      {  /*Go to the first space available*/
+          /*update the counter*/
+          record_count++;
+          memcpy(data, &record_count, sizeof(int));
+          /*move our pointer to the position of the next available record*/
+          data +=sizeof(int);
+          data += ((record_count-1)*record_size);
+          /*write the record*/
+          SR_WriteRecord(data, record);
+          /*changes were made so we make it dirty before the unpin*/
+          BF_Block_SetDirty(block);
+          CALL_OR_DIE(BF_UnpinBlock(block));
+
+          return SR_OK;
+      }
+      /*no changes cause its full, so just unpin it*/
+      CALL_OR_DIE(BF_UnpinBlock(block));
+  }
+
+  /*We need a new Block*/
+
+  /*New Block Allocation*/
+  CALL_OR_DIE(BF_AllocateBlock(fileDesc, block));
+  data = BF_Block_GetData(block);
+  /*write the counter*/
+  int one = 1;
+  memcpy(data, &one, sizeof(int));
+  /*write the record*/
+  data += sizeof(int);
+  SR_WriteRecord(data,record);
+  /*changes were made so we make it dirty before the unpin*/
+  BF_Block_SetDirty(block);
+  CALL_OR_DIE(BF_UnpinBlock(block));
+
   return SR_OK;
 }
 
@@ -112,5 +197,33 @@ SR_ErrorCode SR_SortedFile(
 SR_ErrorCode SR_PrintAllEntries(int fileDesc) {
   // Your code goes here
 
+  int i, j;
+
+  int block_count;
+  int record_count;
+  Record record;
+  char * data;
+
+  CALL_OR_DIE(BF_GetBlockCounter(fileDesc, &block_count));
+  /*we ignore the 0 block cause its the one with the metadata*/
+  for(i = 1; i < block_count; i++)
+  {
+      /*get the block*/
+      CALL_OR_DIE(BF_GetBlock(fileDesc, i, block));
+      data = BF_Block_GetData(block);
+      /*get the record count of this block*/
+      memcpy(&record_count, data, sizeof(int));
+      /*go to 1st record*/
+      data += sizeof(int);
+      for( j = 0; j < record_count; j++ )
+      {
+          SR_ReadRecord(data, &record);
+          SR_PrintRecords(record);
+          /*next record*/
+          data +=record_size;
+      }
+      /*unpin this block as we read all his records*/
+      CALL_OR_DIE(BF_UnpinBlock(block));
+  }
   return SR_OK;
 }
